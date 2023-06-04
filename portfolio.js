13d5e79459e75cabce8aa3aa9f661db91221ff3b
TonyMarshal <nthnuchenna@gmail.com>
2023-06-04 15:24:08 +0100

    update the changes

--- portfolio.js
+++ portfolio.js
@@ -23,3 +23,26 @@ function closeFullImg() {
     fullImgBox.style.display = "none";
 }
 
+/*let css_link = document.getElementById('css')
+
+icon.onclick = function() {
+
+    if (localStorage.getItem("theme") == "index.css") {
+        localStorage.setItem("theme","dark.css")
+        icon.src = "image/sun.png";        localStorage.setItem('css_theme_link','dark.css')
+    }else {
+        localStorage.setItem("theme","index.css")
+        icon.src = "image/moon.png";        localStorage.setItem('css_theme_link','dark.css')
+    }
+    
+    css_link.href = localStorage.getItem('theme')
+   
+}
+
+const change = ()=>{
+    css_link.href = localStorage.getItem('theme')
+
+}
+
+change()*/
+
