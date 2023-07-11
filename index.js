9c0655dfa745130201357c10ebd91ad1233828df
TonyMarshal <nthnuchenna@gmail.com>
2023-07-11 13:38:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let popup = document.getElementById('popup');
 
 var typed = new Typed("#in", {
-    strings: ["Abaikwam M. Anthony.","a Web Developer.","a Software Developer."],
+    strings: ["Abaikwam M. Anthony.","A Web Developer.","A Software Developer."],
     typeSpeed: 70,
     backSpeed: 60,
     loop: true
